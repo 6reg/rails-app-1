440a9d12702dd90566cea6a797457893952ea5eb
6reg <mathiasgreg@gmail.com>
2022-09-23 16:51:11 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,17 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/login', to: 'static_pages#login'
 
+  ################## resources :users ###############################################
+  #  method   URL           Action  Named route          Purpose
+  #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #  GET      /users        index   users_path           page to list all users 
+  #  GET      /users/1      show    user_path(user)      page to show user
+  #  GET      /users/new    new     new_user_path        page to make a new user
+  #  POST     /users        create  users_path           create a new user
+  #  GET      /users/1/edit edit    edit_user_path(user) page to edit user with id 1
+  #  PATCH    /users/1      update  user_path(user)      update user
+  #  DELETE   /users/1      destroy user_path(user)      delete user
+
+  resources :users
+
 end
